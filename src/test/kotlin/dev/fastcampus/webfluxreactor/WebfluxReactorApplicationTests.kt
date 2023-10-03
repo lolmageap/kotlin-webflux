c866d9ac344d4845e03d0562a1bb1978a3b148f1
cherhy <ekxk1234@naver.com>
2023-10-03 22:38:05 +0900

    실행 테스트

--- src/test/kotlin/dev/fastcampus/webfluxreactor/WebfluxReactorApplicationTests.kt
+++ src/test/kotlin/dev/fastcampus/webfluxreactor/WebfluxReactorApplicationTests.kt
@@ -1,13 +1,28 @@
 package dev.fastcampus.webfluxreactor
 
+import dev.fastcampus.webfluxreactor.model.Article
+import dev.fastcampus.webfluxreactor.repository.ArticleRepository
+import mu.KotlinLogging
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
-@SpringBootTest
-class WebfluxReactorApplicationTests {
+private val logger = KotlinLogging.logger {}
 
+@SpringBootTest
+class WebfluxReactorApplicationTests(
+	@Autowired private val articleRepository: ArticleRepository
+) {
 	@Test
 	fun contextLoads() {
+		val count = articleRepository.count().block() ?: 0
+		articleRepository.save( Article(title = "title") ).block()
+		val all = articleRepository.findAll().collectList().block()
+		all?.forEach { logger.debug { it } }
+
+		val currentCount = articleRepository.count().block() ?: 0
+		assertThat(currentCount).isEqualTo(count + 1)
 	}
 
 }
